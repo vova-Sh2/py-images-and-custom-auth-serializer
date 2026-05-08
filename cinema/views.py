e770a46b9a5b4dbb6135819e5487f4df84a6c578
Vova <vovakucin082@gmail.com>
2026-05-08 10:58:17 +0200

    rename the endpoint

--- cinema/views.py
+++ cinema/views.py
@@ -112,7 +112,7 @@ class MovieViewSet(
         methods=["POST"],
         detail=True,
         permission_classes=(IsAdminOrIfAuthenticatedReadOnly,),
-        url_path="upload-movie"
+        url_path="upload-image"
     )
     def upload_image(self, request, pk=None):
         movie = self.get_object()
